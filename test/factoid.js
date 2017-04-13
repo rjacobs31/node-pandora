09122171b43487500647babcc646038ccfd8c5c3
Rudolph Jacobs <jacobsrudolph@yahoo.com>
2017-04-14 01:36:36 +0200

    Add tests for factoid strip.

--- test/factoid.js
+++ test/factoid.js
@@ -1,8 +1,35 @@
 let expect = require('chai').expect;
+let factoids = require('../db/factoids')();
 
 describe('factoids', function() {
   describe('#stripFactoid', function() {
-    it('should strip punctuation from factoid triggers');
+    it('should convert a string to lowercase', function() {
+      expect(factoids.stripFactoid('EnTrOpY')).to.equal('entropy');
+    });
+
+    it('should strip end of string punctuation', function() {
+      expect(factoids.stripFactoid('that is correct!'))
+        .to.equal('that is correct');
+
+      expect(factoids.stripFactoid('hi.'))
+        .to.equal('hi');
+
+      expect(factoids.stripFactoid('is anyone there?'))
+        .to.equal('is anyone there');
+    });
+
+    it('should strip mid-sentence punctuation', function() {
+      expect(factoids.stripFactoid('correct, horse. battery! staple'))
+        .to.equal('correct horse battery staple');
+
+      expect(factoids.stripFactoid('what? how? i don\'t even know anymore'))
+        .to.equal('what how i don\'t even know anymore');
+    });
+
+    it('should leave mid-word symbols', function() {
+      expect(factoids.stripFactoid('loading murder.exe into !mem please wait'))
+        .to.equal('loading murder.exe into !mem please wait');
+    });
   });
 
   describe('#getResponse', function() {
