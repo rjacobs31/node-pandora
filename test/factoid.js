5737e0f951fdbb4c14fb14c225dd1267d8e0718f
Rudolph Jacobs <jacobsrudolph@yahoo.com>
2017-04-14 01:11:07 +0200

    Add initial factoid test file.

---
+++ test/factoid.js
@@ -0,0 +1,11 @@
+let expect = require('chai').expect;
+
+describe('factoids', function() {
+  describe('#stripFactoid', function() {
+    it('should strip punctuation from factoid triggers');
+  });
+
+  describe('#getResponse', function() {
+    it('should get a factoid response when given a trigger');
+  });
+});
