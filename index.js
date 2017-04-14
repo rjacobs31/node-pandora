c795d765d34769641e57a26b2f986b7402749e4f
Rudolph Jacobs <jacobsrudolph@yahoo.com>
2017-04-14 12:08:18 +0200

    Add initial checks for "is" directives.

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 module.exports = function(bp) {
   let factoids = require('./db/factoids')(bp);
+  let addressRegex = /^pan(dora)?:\s*/i;
   bp.middlewares.load();
 
-  bp.hear({platform: "discord", text: /^pandora:/i}, event => {
-    bp.discord.sendText(event.channel.id, "Bananas!");
+  bp.hear({platform: 'discord', text: addressRegex}, event => {
+    let command = event.text.replace(addressRegex, '');
+    let countIs = command.match(/\bis\b/g).length;
+    if (countIs == 1) {
+      bp.discord.sendText(event.channel.id, 'I detected an "is" command.');
+    } else if (countIs > 1) {
+      bp.discord.sendText(event.channel.id, 'Sorry, I don\'t understand.');
+    } else {
+      bp.discord.sendText(event.channel.id, 'Sorry, I don\'t understand.');
+    }
   });
 
-  bp.hear({platform: "discord"}, event => {
+  bp.hear({platform: 'discord'}, event => {
     factoids.getResponse(bp, event)
     .then(responses => {
       if (responses && responses.length > 0) {
